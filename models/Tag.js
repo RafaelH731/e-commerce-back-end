86fabc9bd0bd2bc025e1217789e8ad67520cbf18
Rafael Herrera <rafaelherrera731@gmail.com>
2022-05-18 22:59:54 -0700

    started  the models colums

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,20 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //references product model id(need to do)
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      //references tag model id(need to do)
+    }
   },
   {
     sequelize,
