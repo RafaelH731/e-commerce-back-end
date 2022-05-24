d4cb4077266bb3d901580304f56a13b3e404416f
Rafael Herrera <rafaelherrera731@gmail.com>
2022-05-24 16:45:48 -0700

    working on index.js in models folder

--- models/index.js
+++ models/index.js
@@ -5,7 +5,11 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo
+Product.belongsTo(
+  Category, {
+    foreignKey: "category_id",
+  }
+);
 
 // Categories have many Products
 Category.hasMany
