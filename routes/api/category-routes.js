7252b10d84233867dbecfc6bda26fec7f765b310
Rafael Herrera <rafaelherrera731@gmail.com>
2022-05-28 12:28:59 -0700

    fixed issues connecting to server

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,7 +31,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
@@ -41,7 +41,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
     const category = await Category.findByPk(req.params.id);
@@ -59,7 +59,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const category = await Category.findByPk(req.params.id);
